71b96efbef6f50d565a6526c75017b0879c02961
IgnacioMC20 <jm10cuyun@gmail.com>
2025-07-07 19:48:47 -0600

    feat: add react-scan library and update layout to include script
    chore: replace footer shape image with webp format and remove png
    
    fix: update Footer component to use placeholder for logo and improve structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,13 @@
+import Script from 'next/script';
+
 // src/app/layout.tsx
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+  return <>
+    <Script
+      src="//unpkg.com/react-scan/dist/auto.global.js"
+      crossOrigin="anonymous"
+      strategy="afterInteractive"
+    />
+    {children}
+  </>;
 }
